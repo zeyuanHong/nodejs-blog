bea170231ceda304e934e1f69df7ed7efa3cb5a8
zeyuanHong <hongzeyuan0@gmail.com>
2022-10-16 18:26:47 +0800

    add mysql database

--- app/mysql/mysqlInstance.js
+++ app/mysql/mysqlInstance.js
@@ -4,5 +4,6 @@ const mysql = require("mysql"); // 引入mysql操作的库
 exports.default = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "123456", // 数据库密码
+    password: "123456",
+    database: "blog-data" // 数据库名
 }); // createConnection()会返回一个数据库链接对象，数据库链接对象可以执行各类mysql语句

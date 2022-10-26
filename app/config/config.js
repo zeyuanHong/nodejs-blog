817210d22bcdf7e220dea02120073ab5610f1303
zeyuanHong <hongzeyuan0@gmail.com>
2022-10-26 23:07:17 +0800

    test

--- app/config/config.js
+++ app/config/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.tokenKey = exports.mysqlConfig = void 0;
 exports.mysqlConfig = {
-    host: "http://150.158.188.135",
+    host: "localhost",
     user: "root",
     password: "123456",
     database: "blog_data" // 创建链接的时候默认选中的数据库

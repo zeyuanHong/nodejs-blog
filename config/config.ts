9e7c3ac40c23db69fce907eb3290dfa64c70b6c7
zeyuanHong <hongzeyuan0@gmail.com>
2022-10-26 23:02:46 +0800

    change mysqlConfig

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,5 @@
 export const mysqlConfig = {
-    host: "localhost", // 数据库服务器地址
+    host: "http://150.158.188.135", // 数据库服务器地址
     user: "root", // 数据库用户名
     password: "123456", // 数据库密码
     database: "blog_data" // 创建链接的时候默认选中的数据库
